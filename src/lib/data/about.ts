44c8a0a232f7f937115cd640fd14ebc87d4ac4ce
cazdlt <cazdlt@gmail.com>
2022-09-13 18:41:02 -0500

    add frases api route and fetch

---
+++ src/lib/data/about.ts
@@ -0,0 +1,6 @@
+export const about = {
+    summary: "Camilo Andrés Zamora de la Torre, 26, Colombiano",
+	descriptionItems: ['colombiano'],
+	learning: [{ what: 'svelte', why: ['this site'] }],
+	frases: ["hola", "chao"]
+};
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,52 @@
-<script>
+<script lang="ts">
+	import PageTitle from '$lib/components/pageTitle.svelte';
+	import { about } from '$lib/data/about';
 
-import PageTitle from "$lib/components/pageTitle.svelte";
+	const { descriptionItems, learning, summary } = about;
 
+	const getFrase = async () => {
+		let response = await fetch("/api/whatTheySay")
+		let result = await response.json()
+		return JSON.stringify(result);
+	};
+
+	let whatTheySayPromise: Promise<string>;
+	const handleButton = () => {
+		whatTheySayPromise = getFrase();
+	};
 </script>
 
 <PageTitle title="About" />
+<div class="italic">{summary}</div>
+
+<div>
+	<ul class="list-disc list-inside">
+		{#each descriptionItems as description}
+			<li>{description}</li>
+		{/each}
+	</ul>
+</div>
+
+<div>
+	{#each learning as skill}
+		<span class="font-bold">{skill.what}</span>
+		<ul>
+			{#each skill.why as idea}
+				<li>{idea}</li>
+			{/each}
+		</ul>
+	{/each}
+</div>
+
+<div>
+	<button on:click={handleButton}> ¿Qué dicen de mí? </button>
+	{#await whatTheySayPromise}
+		...
+	{:then whatTheySay}
+		{#if !!whatTheySay}
+			{whatTheySay}
+		{/if}
+	{:catch error}
+		<p class="text-red-600">{error.message}</p>
+	{/await}
+</div>
