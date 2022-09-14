bc51cdc8e7e88652df2fac65bc3a19e268306495
cazdlt <cazdlt@gmail.com>
2022-09-13 19:16:31 -0500

    basic about page style

--- src/lib/data/about.ts
+++ src/lib/data/about.ts
@@ -1,6 +1,48 @@
 export const about = {
-    summary: "Camilo Andrés Zamora de la Torre, 26, Colombiano",
-	descriptionItems: ['colombiano'],
-	learning: [{ what: 'svelte', why: ['this site'] }],
-	frases: ["hola", "chao"]
+	summary: 'Camilo Andrés Zamora de la Torre, 26, Colombiano',
+	descriptionItems: [
+		'Ingeniero electrónico que nunca supo qué estudiar',
+		'Le gusta la música',
+		'Practica varios instrumentos, pero ninguno muy bien',
+		'Intenta bailar dos veces por semana',
+		'Nació en Cali, vive en Bogotá',
+		'Tiene dos gatos',
+		'Camina lento pero se mueve en bicicleta',
+		'Científico de datos actualmente trabajando en Mercadolibre'
+	],
+	learning: [
+		{
+			what: 'Deep Learning (NYU Lecun / Canziani)',
+			why: [
+				'Scheduler para red con SIC Cooperativo',
+				'Bot de Tetris NES',
+				'Separación de instrumentos'
+			]
+		},
+		{
+			what: 'React.js + Next.js',
+			why: [
+				'Visualizaciones para Last.fm',
+				'Mezclar playlist y <i>true random</i> en Spotify',
+				'kolibrika.co'
+			]
+		},
+		{
+			what: 'Flutter',
+			why: ['Es increíble que no hayan apps gratis buenas para rutinas de bodyweight']
+		}
+	],
+	frases: [
+		'"Muy lindo." - Mi mamá',
+		'"Excelente profesional." - Mi jefe, probablemente',
+		'"Buen muchacho." - Mi abuela',
+		'"非常に面白い" - ののか',
+		'"¿En qué le puedo ayudar?" - El chatbot de Claro',
+		'"Muy cumplido y responsable." - La DIAN',
+		'"Aprende rápido." - Stack Overflow',
+		'"Tiene buen gusto." - El algoritmo de Spotify',
+		'"¿Quién?" - Mis compañeros de primaria',
+		'"Muy malos chistes. - Mis compañeros de universidad"',
+		'"Sigue ahí." - Netflix'
+	]
 };
