bd9a15583d58686212840581208bbc96f87df5d1
cazdlt <cazdlt@gmail.com>
2022-09-18 19:23:52 -0500

    responsive layout

--- src/lib/components/sidebar.svelte
+++ src/lib/components/sidebar.svelte
@@ -6,7 +6,7 @@
 	const subpages = pages.filter((p) => !p.mainSidebar);
 </script>
 
-<div class="flex flex-col bg-light min-h-full px-8 items-end justify-center text-right">
+<div class="flex flex-col bg-light min-h-full px-8 py-16 lg:items-end justify-center text-center lg:text-right">
 	<a href="/" class="text-3xl text-primary my-2">{site.author.name.toLowerCase()}</a>
 	{#if 'job' in site.author}
 		<span class="text-sm">{site.author.job?.toLowerCase()}</span>
